0ecc605fca45b242824fd69a8650b0c9c425a95c
Harbison Apps <harbisonapps@gmail.com>
2020-02-21 07:56:13 -0800

    Added vue-material

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,33 +1,71 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="md-layout md-gutter md-alignment-center">
+    <md-card md-with-hover>
+    
+        <md-card-media-cover md-text-scrim>
+          <md-card-media md-big>
+          <img src="https://i.imgur.com/0umadnY.jpg" alt="Burger">
+        </md-card-media>
+        <md-card-area>
+        <md-card-header class="md-title">
+          Card Header
+        </md-card-header>
+        <md-card-content>
+          Lorem ipsum dolor sit amet sdvpw consectetur adipisicing elit!
+        </md-card-content>
+        
+        </md-card-area>
+        </md-card-media-cover>
+        <md-card-actions>
+          <md-button class="edit">Edit</md-button>
+          <md-button class="delete">Delete</md-button>
+        </md-card-actions>
+      
+    </md-card>
+    <md-card md-with-hover>
+    
+        <md-card-header class="md-title">
+          Card Header
+        </md-card-header>
+        <md-card-content>
+          Card Content
+        </md-card-content>
+        <md-card-actions>
+          <md-button class="fullBtn">More</md-button>
+        </md-card-actions>
+      
+    </md-card>
+    <md-card md-with-hover>
+    
+        <md-card-header class="md-title">
+          Card Header
+        </md-card-header>
+        <md-card-content>
+          Card Content
+        </md-card-content>
+        <md-card-actions>
+          <md-button class="fullBtn">More</md-button>
+        </md-card-actions>
+      
+    </md-card>
+    <md-card md-with-hover>
+    
+        <md-card-header class="md-title">
+          Card Header
+        </md-card-header>
+        <md-card-content>
+          Card Content
+        </md-card-content>
+        <md-card-actions>
+          <md-button class="fullBtn">More</md-button>
+        </md-card-actions>
+      
+    </md-card>
   </div>
+  </div>
+
 </template>
 
 <script>
@@ -54,5 +92,40 @@ li {
 }
 a {
   color: #42b983;
+}
+.md-card{
+  width: 300px;
+  max-height: 500px;
+  border: rgba(175, 175, 175, 0.5) solid .5px;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  display: inline-block;
+}
+.md-card-header{
+  text-align: center
+}
+.md-card-content{
+  text-align: left;
+  text-indent: 6px;
+}
+.md-button{
+  color: green;
+  border: solid 1px;
+  border-color: green;
+  border-radius: 5px;
+  
+}
+.fullBtn{
+  width: 300px
+}
+.edit{
+  color: blue;
+  border-color: blue
+}
+.delete{
+  background: maroon;
+  color: white;
+  border-color: transparent
 }
 </style>
